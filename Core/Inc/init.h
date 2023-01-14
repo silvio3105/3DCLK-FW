98c464f450c2da6255e3814e9153bdf2966b6932
silvio3105 <104866334+silvio3105@users.noreply.github.com>
2023-01-14 23:56:56 +0100

    Fixes with comments.

--- Core/Inc/init.h
+++ Core/Inc/init.h
@@ -2,14 +2,31 @@
  * @file init.h
  * @author silvio3105 (www.github.com/silvio3105)
  * @brief Header file with CubeMX configuration.
- * @date 07.01.2023
  * 
- * @copyright Copyright (c) 2023
+ * @copyright Copyright (c) 2023, silvio3105
  * 
  */
 
-#ifndef _AAA_H
-#define _AAA_H
+/*
+License
+
+Copyright (c) 2023, silvio3105 (www.github.com/silvio3105)
+
+Access and use of this Project and its contents are granted free of charge to any Person.
+The Person is allowed to copy, modify and use The Project and its contents only for non-commercial use.
+Commercial use of this Project and its contents is prohibited.
+Modifying this License and/or sublicensing is prohibited.
+
+THE PROJECT AND ITS CONTENT ARE PROVIDED "AS IS" WITH ALL FAULTS AND WITHOUT EXPRESSED OR IMPLIED WARRANTY.
+THE AUTHOR KEEPS ALL RIGHTS TO CHANGE OR REMOVE THE CONTENTS OF THIS PROJECT WITHOUT PREVIOUS NOTICE.
+THE AUTHOR IS NOT RESPONSIBLE FOR DAMAGE OF ANY KIND OR LIABILITY CAUSED BY USING THE CONTENTS OF THIS PROJECT.
+
+This License shall be included in all methodal textual files.
+*/
+
+
+#ifndef _INIT_H
+#define _INIT_H
 
 // ----- INCLUDE FILES
 #include			"config.h"
